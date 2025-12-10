0913381e6aac12e026113542bbb2dc888c3684ee
RYGB1204 <green.tea.mm62@gmail.com>
2025-12-10 11:22:52 +0900

    2025/12/10 11:21:35.22

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ const Footer = document.querySelector("footer");
 
 Header.animate(KeyframesHeader, Options);
 Footer.animate(KeyframesFooter, Options);
+
+const ButtonOption = document.querySelector("#ButtonOption");
+const MenuOption = document.querySelector("#MenuOption");
+const CrabY = document.querySelector("#CrabY");
+const CrabR = document.querySelector("#CrabR");
+const CrabP = document.querySelector("#CrabP");
+const CrabB = document.querySelector("#CrabB");
+const CrabG = document.querySelector("#CrabG");
+
+ButtonOption.addEventListener("click", function() {
+    MenuOption.classList.toggle("active");
+    CrabY.classList.toggle("active");
+    CrabR.classList.toggle("active");
+    CrabP.classList.toggle("active");
+    CrabB.classList.toggle("active");
+    CrabG.classList.toggle("active");
+});
