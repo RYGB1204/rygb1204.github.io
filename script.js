4c7e6368723d689003b3e1af512a08564a967483
RYGB1204 <green.tea.mm62@gmail.com>
2025-12-13 23:59:17 +0900

    2025/12/13 23:59:07.05

--- script.js
+++ script.js
@@ -128,12 +128,36 @@ ButtonOption.addEventListener("click", function() {
 
 //ChikenStart
 
+let IfYouOperateCanCannot = false;
+
 const Chicken = document.querySelector("#Chicken");
 const StyleChicken = getComputedStyle(Chicken);
 
 let XChiken, YChiken, WidthChiken;
 let ScaleChiken;
+
+const KeyframesChickenAppear = {
+    translate: ["0 -100vh", 0],
+};
+const OptionsChickenAppear = {
+    duration: 1000,
+    easing: "ease-in"
+};
+
+Chicken.animate(KeyframesChickenAppear, OptionsChickenAppear);
+
 let KeyframesChikenLeft, KeyframesChickenRight;
+
+const KeyframesChickenAttacked = {
+    rotate: ["0deg", "360deg"]
+};
+
+const OptionsChickenAttacked = {
+    duration: 200,
+    iterations: "Infinity"
+};
+
+let IfChickenAttacked = false;
 
 function Apdate() {
 
@@ -149,9 +173,38 @@ function Apdate() {
         scale: [`${ScaleChiken}`, "+1 1"]
     };
 
+    for (const Object of ArrayCrab) {
+
+        const StyleCrab = getComputedStyle(Object);
+
+        const XCrub = Number.parseFloat(StyleCrab.getPropertyValue("left")) + Number.parseFloat(StyleCrab.getPropertyValue("width")) / 2;
+        const YCrub = Number.parseFloat(StyleCrab.getPropertyValue("bottom")) + Number.parseFloat(StyleCrab.getPropertyValue("width")) / 2;//console.log(YCrub);
+
+        if (!IfChickenAttacked) {
+
+            if (Math.abs(XChiken - XCrub) < WidthChiken && Math.abs(YChiken - YCrub) < WidthChiken) {
+
+                if ((XChiken - XCrub) ** 2 + (YChiken - YCrub) ** 2 < WidthChiken ** 2) {
+
+                    Chicken.animate(KeyframesChickenAttacked, OptionsChickenAttacked);
+
+                    IfChickenAttacked = true;
+
+                }
+                
+            }
+
+        }
+
+    }
+
 }
 
-setInterval(Apdate, 1);
+setInterval(Apdate, 5);
+
+setInterval(() => {
+    console.log(ArrayCrab);
+}, 1000);
 
 let TimePreLeft, TimePreRight;
 let TimeStartJump;
@@ -174,7 +227,7 @@ function MoveLeft(TimeNow) {
 
     TimePreLeft = TimeNow;
 
-    if (IfKeyLeftOnOff) {
+    if (IfYouOperateCanCannot && IfKeyLeftOnOff) {
         requestAnimationFrame(MoveLeft);
     }
 }
@@ -197,7 +250,7 @@ function MoveRight(TimeNow) {
 
     TimePreRight = TimeNow;
 
-    if (IfKeyRightOnOff) {
+    if (IfYouOperateCanCannot && IfKeyRightOnOff) {
         requestAnimationFrame(MoveRight);
     }
 }
@@ -243,7 +296,7 @@ let IfJumpOnOff = false;
 
 function Keydown(event) {
 
-    if (!event.repeat) {
+    if (IfYouOperateCanCannot && !event.repeat) {
 
         if (event.key === "ArrowLeft") {
             IfKeyLeftOnOff = true;
@@ -267,13 +320,17 @@ function Keydown(event) {
 
 function Keyup(event) {
 
-    if (event.key === "ArrowLeft") {
-        IfKeyLeftOnOff = false;
-        TimePreLeft = undefined;
-    }
-    if (event.key === "ArrowRight") {
-        IfKeyRightOnOff = false;
-        TimePreRight = undefined;
+    if (IfYouOperateCanCannot) {
+
+        if (event.key === "ArrowLeft") {
+            IfKeyLeftOnOff = false;
+            TimePreLeft = undefined;
+        }
+        if (event.key === "ArrowRight") {
+            IfKeyRightOnOff = false;
+            TimePreRight = undefined;
+        }
+
     }
     
 }
@@ -288,35 +345,74 @@ const Contents = document.querySelector("#Contents");
 
 const ArrayCrab = [];
 
-// function JumpCrab(TimeNow) {
+const Object = {
 
-//     const VelocityInitial = WidthChiken * 15;
-//     const Gravity = WidthChiken * 30;
+    MoveCrabX:
+    function MoveCrabX(TimeNow) {
 
-//     if (TimeStartJump === undefined) {
-//         TimeStartJump = TimeNow;
-//     }
+        const StyleCrab = getComputedStyle(this.Crab);
 
-//     let TimeElapsedJump = TimeNow - TimeStartJump;
-//     let TimeElapsedJumpSecond = Math.min(TimeElapsedJump / 1000, 1);
+        if (this.TimeStartX === undefined) {
+        this.TimeStartX = TimeNow;
+        }
 
-//     if (Number.isNaN(TimeElapsedJumpSecond)) {
-//         TimeElapsedJumpSecond = 0;
-//     }
+        let TimeElapsed = TimeNow - this.TimeStartX;
 
-//     const DistanceJump = VelocityInitial * TimeElapsedJumpSecond - Gravity * TimeElapsedJumpSecond ** 2 / 2;
+        if (Number.isNaN(TimeElapsed)) {
+            TimeElapsed = 0;
+        }
 
-//     Chicken.style.bottom = `${DistanceJump}px`;
+        let DistanceMove;
 
-//     if (0 <= TimeElapsedJumpSecond && TimeElapsedJumpSecond < 1) {
-//         requestAnimationFrame(Jump);
-//     }
-//     else {
-//         IfJumpOnOff = false;
-//         TimeStartJump = undefined;
-//     }
+        if (this.Crab.classList.contains("CrabP")) {
+            DistanceMove = TimeElapsed * WidthChiken / 30;
+        }
+        else {
+            DistanceMove = TimeElapsed * WidthChiken / 300;
+        }
 
-// }
+        if (this.Crab.classList.contains("Left")) {
+            this.Crab.style.right = `${Number.parseFloat(StyleCrab.getPropertyValue("right")) - DistanceMove}px`;
+        }
+        if (this.Crab.classList.contains("Right")) {
+            this.Crab.style.left = `${Number.parseFloat(StyleCrab.getPropertyValue("left")) - DistanceMove}px`;
+        }
+
+        this.TimeStartX = TimeNow;
+
+        requestAnimationFrame(MoveCrabX.bind(this));
+
+    },
+
+    MoveCrabY: 
+    function MoveCrabY(TimeNow) {
+
+        const VelocityInitial = WidthChiken * this.VelocityInitialRate;
+        const Gravity = WidthChiken * this.VelocityInitialRate * this.GravityRate;
+
+        if (!this.TimeStartY) {
+            this.TimeStartY = TimeNow;
+        }
+
+        let TimeElapsed = TimeNow - this.TimeStartY;
+        let TimeElapsedSecond = Math.min(TimeElapsed / 1000, 2 / this.GravityRate);
+
+        if (Number.isNaN(TimeElapsedSecond)) {
+            TimeElapsedSecond = 0;
+        }
+
+        const DistanceJump = VelocityInitial * TimeElapsedSecond - Gravity * TimeElapsedSecond ** 2 / 2;
+        
+        this.Crab.style.bottom = `${DistanceJump}px`;
+
+        if (2 / this.GravityRate <= TimeElapsedSecond) {
+            this.TimeStartY = undefined;
+        }
+
+        requestAnimationFrame(MoveCrabY.bind(this));
+
+    }
+};
 
 const KeyframesCrabLeft = {
     translate: [0, "+130vw 0"]
@@ -338,60 +434,104 @@ function DisplayCrab(Crab) {
 
     Contents.append(Crab);
 
-    ArrayCrab.push(Crab);
+    const ObjectCrab = {
+        Crab: Crab,
+        TimeStartX: undefined,
+        TimeStartY: undefined,
+        VelocityInitialRate: undefined,
+        GravityRate: undefined,
+    };
 
     switch (Crab.className) {
 
-        case "CrabR":
-            Crab.animate(KeyframesCrabLeft, OptionsCrab);
+        case "CrabR Left":
+
+            // Crab.animate(KeyframesCrabLeft, OptionsCrab);
+
+            ObjectCrab.VelocityInitialRate = 10;
+            ObjectCrab.GravityRate = 4;
+
         break;
-        case "CrabB":
-            Crab.animate(KeyframesCrabRight, OptionsCrab);
+        case "CrabB Right":
+
+            // Crab.animate(KeyframesCrabRight, OptionsCrab);
+
+            ObjectCrab.VelocityInitialRate = 10;
+            ObjectCrab.GravityRate = 4;
+
         break;
-        case "CrabY":
-            Crab.animate(KeyframesCrabLeft, OptionsCrab);
+        case "CrabY Left":
+
+            // Crab.animate(KeyframesCrabLeft, OptionsCrab);
+
+            ObjectCrab.VelocityInitialRate = 8;
+            ObjectCrab.GravityRate = 1;
+
         break;
-        case "CrabG":
-            Crab.animate(KeyframesCrabRight, OptionsCrab);
+        case "CrabG Right":
+
+            // Crab.animate(KeyframesCrabRight, OptionsCrab);
+
+            ObjectCrab.VelocityInitialRate = 8;
+            ObjectCrab.GravityRate = 1;
+
         break;
         case "CrabP Left":
-            Crab.animate(KeyframesCrabLeft, OptionsCrabFast);
+
+            // Crab.animate(KeyframesCrabLeft, OptionsCrabFast);
+
+            ObjectCrab.VelocityInitialRate = 0;
+            ObjectCrab.GravityRate = 0;
+
         break;
         case "CrabP Right":
-            Crab.animate(KeyframesCrabRight, OptionsCrabFast);
+
+            // Crab.animate(KeyframesCrabRight, OptionsCrabFast);
+
+            ObjectCrab.VelocityInitialRate = 0;
+            ObjectCrab.GravityRate = 0;
+
         break;
     }
 
+    const FuncX = Object.MoveCrabX.bind(ObjectCrab);
+    const FuncY = Object.MoveCrabY.bind(ObjectCrab);
+    FuncX();
+    setTimeout(FuncY, Math.random() * 2000)
+
+    ArrayCrab.push(Crab);
+
     setTimeout(() => {
+        ArrayCrab.shift();
         Crab.remove();
-    }, 5000);
+    }, 7000);
 
 }
 
 function SpawnCrabR(Crab) {
 
-    Crab.classList.add("CrabR");
+    Crab.classList.add("CrabR", "Left");
 
     DisplayCrab(Crab);
 
 }
 function SpawnCrabB(Crab) {
 
-    Crab.classList.add("CrabB");
+    Crab.classList.add("CrabB", "Right");
 
     DisplayCrab(Crab);
 
 }
 function SpawnCrabY(Crab) {
 
-    Crab.classList.add("CrabY");
+    Crab.classList.add("CrabY", "Left");
 
     DisplayCrab(Crab);
 
 }
 function SpawnCrabG(Crab) {
 
-    Crab.classList.add("CrabG");
+    Crab.classList.add("CrabG", "Right");
 
     DisplayCrab(Crab);
 
@@ -444,7 +584,18 @@ const IntervalChooseCrab = setInterval(ChooseCrab, 1000);
 //EnemyCrabEnd
 //CountdouwStart
 
+let IntervalCountdown;
+
 const CountText = document.querySelector("#CountText");
+
+const KeyframesCountdown = {
+    offset: [0.8, 1],
+    scale: [1, 1.5]
+}
+
+const OptionsCountdown = {
+    duration: 1000
+}
 
 let CountInt = 31;
 
@@ -452,18 +603,26 @@ function Countdown() {
 
     CountInt--;
 
+    const AnimateCountText = CountText.animate(KeyframesCountdown, OptionsCountdown);
+
     if (0 < CountInt) {
         CountText.textContent = CountInt;
     }
     else {
+        IfYouOperateCanCannot = false;
         CountText.textContent = "Finish!"
+        AnimateCountText.cancel();
         clearInterval(IntervalChooseCrab);
         clearInterval(IntervalCountdown);
     }
     
-
 }
 
-const IntervalCountdown = setInterval(Countdown, 1000);
+setTimeout(() => {
+    IfYouOperateCanCannot = true;
+    IntervalCountdown = setInterval(Countdown, 1000);
+}, 1500);
+
+//const IntervalCountdown = setTimeout(setInterval(Countdown, 1000), 5000);
 
 //CountdownEnd
