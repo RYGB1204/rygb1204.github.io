4e7ebcfd17dcb6c85b4bc8e9a4a59aa0921dfb1d
RYGB1204 <green.tea.mm62@gmail.com>
2025-12-14 23:53:17 +0900

    2025/12/14 23:53:08.52

---
+++ JavaScript/Chicken.js
@@ -0,0 +1,254 @@
+const Chicken = document.querySelector("#Chicken");
+const StyleChicken = getComputedStyle(Chicken);
+
+let XChiken, YChiken, WidthChiken;
+
+const TranslateChicken = StyleChicken.getPropertyValue("translate");
+let ScaleChicken;
+
+let XCrub, YCrub;
+
+const KeyframesChickenAppear = {
+    translate: ["0 -100vh", 0],
+};
+const OptionsChickenAppear = {
+    duration: 1000,
+    easing: "ease-in"
+};
+
+Chicken.animate(KeyframesChickenAppear, OptionsChickenAppear);
+
+let KeyframesChikenLeft, KeyframesChickenRight;
+
+const KeyframesChickenAttackedTranslate = {
+    translate: []
+};
+const KeyframesChickenAttackedRotate = {
+    rotate: ["0deg", "360deg"]
+};
+
+const KeyframesChickenSuccess = {
+    translate: []
+};
+
+const OptionsChickenAttackedTranslate = {
+    duration: 2000,
+    fill: "both"
+
+};
+const OptionsChickenAttackedRotate = {
+    duration: 200,
+    iterations: "Infinity"
+
+};
+
+const OptionsChickenSuccess = {
+    duration: 2000,
+    fill: "both"
+}
+
+function FlickChicken() {
+
+    if (XChiken - XCrub < 0) {
+        KeyframesChickenAttackedTranslate.translate = [0, "-200vw -100vw"];
+    }
+    else {
+        KeyframesChickenAttackedTranslate.translate = [0, "+200vw -100vw"];
+    }
+
+    const AnimateChickenAttacked = Chicken.animate(KeyframesChickenAttackedTranslate, OptionsChickenAttackedTranslate);
+    Chicken.animate(KeyframesChickenAttackedRotate, OptionsChickenAttackedRotate);
+
+    StateGame = ObjectStateGame.Failure;
+
+    AnimateChickenAttacked.finished.then(() => {
+        DisplayLose();
+    })
+}
+
+function FlyChicken() {
+
+    if (ScaleChicken.split(" ")[0] === "-1") {
+        KeyframesChickenSuccess.translate = [0, "-10vw -100vh"];
+    }
+    else {
+        KeyframesChickenSuccess.translate = [0, "+10vw -100vh"];
+    }
+
+    Chicken.animate(KeyframesChickenSuccess, OptionsChickenSuccess);
+
+}
+
+function Apdate() {
+
+    XChiken = Number.parseFloat(StyleChicken.getPropertyValue("left")) + Number.parseFloat(StyleChicken.getPropertyValue("width")) / 2;//console.log(XNow);
+    YChiken = Number.parseFloat(StyleChicken.getPropertyValue("bottom")) + Number.parseFloat(StyleChicken.getPropertyValue("width")) / 2;//console.log(YNow);
+    WidthChiken = Number.parseFloat(StyleChicken.getPropertyValue("width"));
+
+    ScaleChicken = StyleChicken.getPropertyValue("scale");
+
+    KeyframesChikenLeft = {
+        scale: [`${ScaleChicken}`, "-1 1"]
+    };
+    KeyframesChickenRight = {
+        scale: [`${ScaleChicken}`, "+1 1"]
+    };
+
+    for (const Crub of ArrayCrab) {
+
+        const StyleCrab = getComputedStyle(Crub);
+
+        XCrub = Number.parseFloat(StyleCrab.getPropertyValue("left")) + Number.parseFloat(StyleCrab.getPropertyValue("width")) * 0.50;
+        YCrub = Number.parseFloat(StyleCrab.getPropertyValue("bottom")) + Number.parseFloat(StyleCrab.getPropertyValue("width")) * 0.32;//console.log(YCrub);
+
+        if (StateGame === ObjectStateGame.AfterStart) {
+
+            if (Math.abs(XChiken - XCrub) < WidthChiken && Math.abs(YChiken - YCrub) < WidthChiken) {
+
+                if ((XChiken - XCrub) ** 2 + (YChiken - YCrub) ** 2 < WidthChiken ** 2) {
+
+                    FlickChicken();
+
+                }
+                
+            }
+
+        }
+
+    }
+
+}
+
+setInterval(Apdate, 5);
+
+let TimePreLeft, TimePreRight;
+let TimeStartJump;
+
+function MoveLeft(TimeNow) {
+
+    if (TimePreLeft === undefined) {
+        TimePreLeft = TimeNow;
+    }
+
+    let TimeElapsedLeft = TimeNow - TimePreLeft;
+
+    if (Number.isNaN(TimeElapsedLeft)) {
+        TimeElapsedLeft = 0;
+    }
+
+    const DistanceMoveLeft = TimeElapsedLeft * WidthChiken / 200;
+
+    Chicken.style.left = `${XChiken - WidthChiken / 2 - DistanceMoveLeft}px`;
+
+    TimePreLeft = TimeNow;
+
+    if (StateGame === ObjectStateGame.AfterStart && IfKeyLeftOnOff) {
+        requestAnimationFrame(MoveLeft);
+    }
+}
+
+function MoveRight(TimeNow) {
+
+    if (TimePreRight === undefined) {
+        TimePreRight = TimeNow;
+    }
+
+    let TimeElapsedRight = TimeNow - TimePreRight;
+
+    if (Number.isNaN(TimeElapsedRight)) {
+        TimeElapsedRight = 0;
+    }
+
+    const DistanceMoveRight = TimeElapsedRight * WidthChiken / 200;
+
+    Chicken.style.left = `${XChiken - WidthChiken / 2 + DistanceMoveRight}px`;
+
+    TimePreRight = TimeNow;
+
+    if (StateGame === ObjectStateGame.AfterStart && IfKeyRightOnOff) {
+        requestAnimationFrame(MoveRight);
+    }
+}
+
+function Jump(TimeNow) {
+
+    const VelocityInitial = WidthChiken * 12;
+    const Gravity = WidthChiken * 24;
+
+    if (TimeStartJump === undefined) {
+        TimeStartJump = TimeNow;
+    }
+
+    let TimeElapsedJump = TimeNow - TimeStartJump;
+    let TimeElapsedJumpSecond = Math.min(TimeElapsedJump / 1000, 1);
+
+    if (Number.isNaN(TimeElapsedJumpSecond)) {
+        TimeElapsedJumpSecond = 0;
+    }
+
+    const DistanceJump = VelocityInitial * TimeElapsedJumpSecond - Gravity * TimeElapsedJumpSecond ** 2 / 2;
+
+    Chicken.style.bottom = `${DistanceJump}px`;console.log(Number.parseInt(TranslateChicken.split(" ")[1]));
+
+    if (StateGame !== ObjectStateGame.Failure && 0 <= TimeElapsedJumpSecond && TimeElapsedJumpSecond < 1) {
+        requestAnimationFrame(Jump);
+    }
+    else {
+        IfJumpOnOff = false;
+        TimeStartJump = undefined;
+    }
+
+}
+
+const OptionsChicken = {
+    duration: 100,
+    fill: "both"
+};
+
+let IfKeyLeftOnOff = false;
+let IfKeyRightOnOff = false;
+let IfJumpOnOff = false;
+
+function Keydown(event) {
+
+    if (StateGame === ObjectStateGame.AfterStart && !event.repeat) {
+
+        if (event.key === "ArrowLeft") {
+            IfKeyLeftOnOff = true;
+            MoveLeft();
+            Chicken.animate(KeyframesChikenLeft, OptionsChicken);
+        }
+        if (event.key === "ArrowRight") {
+            IfKeyRightOnOff = true;
+            MoveRight();
+            Chicken.animate(KeyframesChickenRight, OptionsChicken);
+        }
+
+        if (event.key === " " && !IfJumpOnOff)  {
+            IfJumpOnOff = true;
+            Jump();
+        }
+
+    }
+
+}
+
+function Keyup(event) {
+
+    if (StateGame === ObjectStateGame.AfterStart) {
+
+        if (event.key === "ArrowLeft") {
+            IfKeyLeftOnOff = false;
+            TimePreLeft = undefined;
+        }
+        if (event.key === "ArrowRight") {
+            IfKeyRightOnOff = false;
+            TimePreRight = undefined;
+        }
+
+    }
+    
+}
+
+document.addEventListener("keydown", Keydown);
+document.addEventListener("keyup", Keyup);
---
+++ JavaScript/Countdown.js
@@ -0,0 +1,78 @@
+const CountText = document.querySelector("#CountText");
+
+const KeyframesCountdown = {
+    offset: [0.8, 1],
+    scale: [1, 1.5]
+}
+const OptionsCountdown = {
+    duration: 1000
+}
+
+const AnimateCountText = CountText.animate(KeyframesCountdown, OptionsCountdown);
+AnimateCountText.cancel();
+
+let IntervalCountdown;
+let CountInt = 31;
+
+function Countdown() {
+
+    CountInt--;
+
+    if (StateGame === ObjectStateGame.Failure) {
+
+        clearInterval(IntervalCountdown);
+        
+    }
+    else if (0 < CountInt) {
+
+        CountText.textContent = CountInt;
+        AnimateCountText.play();
+
+    }
+    else {
+
+        StateGame = ObjectStateGame.Success
+
+        CountText.textContent = "Finish!"
+        
+        clearInterval(IntervalCountdown);
+
+        clearInterval(IntervalChooseCrab);
+
+        setTimeout(DisplayWin, 1000);
+
+        setTimeout(FlyChicken, 1000);
+
+    }
+    
+}
+
+setTimeout(() => {
+    StateGame = ObjectStateGame.AfterStart
+    IntervalCountdown = setInterval(Countdown, 1000);
+}, 1500);
+
+const KeyframesLoseText = {
+    scale: [1, 2]
+}
+const KeyframesWinText = {
+    scale: [1, 2]
+}
+
+const OptionsLoseText = {
+    duration: 200,
+    fill: "both"
+}
+const OptionsWinText = {
+    duration: 200,
+    fill: "both"
+}
+
+function DisplayLose() {
+    CountText.textContent = "YOU LOSE"
+    CountText.animate(KeyframesLoseText, OptionsLoseText);
+}
+function DisplayWin() {
+    CountText.textContent = "YOU WIN!"
+    CountText.animate(KeyframesWinText, OptionsWinText);
+}
