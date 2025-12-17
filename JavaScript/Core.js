d913f946a18c7a788e475e7df2f2b7d8dd673063
RYGB1204 <green.tea.mm62@gmail.com>
2025-12-17 23:17:38 +0900

    2025/12/17 23:17:29.59

--- JavaScript/Core.js
+++ JavaScript/Core.js
@@ -1,16 +1,63 @@
-const ObjectStateGame = {
-    BeforeStart: "BeforeStart",
-    AfterStart: "AfterStart",
-    Failure: "Failure",
-    Success: "Success"
-};
+let WidthView = document.documentElement.clientWidth;
+let HeightView = document.documentElement.clientHeight;
+let AspectRatio = WidthView / HeightView;
 
-let StateGame = ObjectStateGame.BeforeStart;
-const StateGamePrevious = {
-    CountdownJS: "",
-    ChickenJS: "",
-    CrabJS: ""
-};
+function LoadModeA() {
+
+    const CssModeA = document.createElement("link");
+    CssModeA.rel = "stylesheet";
+    CssModeA.href = "../CSS/ModeA.css";
+    document.head.append(CssModeA);
+
+    const FileModeA = document.createElement("script");
+    FileModeA.src = "JavaScript/ModeA/ModeA.js";
+    document.head.append(FileModeA);
+
+    const FileCountdown = document.createElement("script");
+    FileCountdown.src = "JavaScript/ModeA/Countdown.js";
+    document.head.append(FileCountdown);
+
+    const FileChicken = document.createElement("script");
+    FileChicken.src = "JavaScript/ModeA/Chicken.js";
+    document.head.append(FileChicken);
+
+    const FileCrab = document.createElement("script");
+    FileCrab.src = "JavaScript/ModeA/Crab.js";
+    document.head.append(FileCrab);
+
+}
+
+function CheckMode() {
+
+    if (1 / 1 <= AspectRatio && AspectRatio <= 2 / 1) {
+
+        if (600 < HeightView) {
+
+            return "ModeA";
+
+        }
+
+    }
+    else if (AspectRatio < 1 / 1) {
+
+        
+
+    }
+    else {
+
+    }
+
+}
+
+const Mode = CheckMode();
+
+switch (Mode) {
+
+    case "ModeA":
+        LoadModeA();
+        break;
+
+}
 
 const Body = document.querySelector("body");
 
@@ -19,15 +66,23 @@ const StyleContents = getComputedStyle(Contents);
 
 let WidthContents;
 
-const ArrayCrab = [];
+
 
 function ApdateCoreJS() {
 
+    WidthView = document.documentElement.clientWidth;
+    HeightView = document.documentElement.clientHeight;
+    AspectRatio = WidthView / HeightView;
+
     WidthContents = Number.parseFloat(StyleContents.getPropertyValue("width"));
+
+    if (Mode !== CheckMode()) {
+        window.location.reload();
+    }
     
 }
 
-setInterval(ApdateCoreJS, 10);
+setInterval(ApdateCoreJS, 100);
 
 addEventListener("load", () => {
     Body.style.visibility = "visible";
---
+++ JavaScript/ModeA/ModeA.js
@@ -0,0 +1,15 @@
+const ObjectStateGame = {
+    BeforeStart: "BeforeStart",
+    AfterStart: "AfterStart",
+    Failure: "Failure",
+    Success: "Success"
+};
+
+let StateGame = ObjectStateGame.BeforeStart;
+const StateGamePrevious = {
+    CountdownJS: "",
+    ChickenJS: "",
+    CrabJS: ""
+};
+
+const ArrayCrab = [];
